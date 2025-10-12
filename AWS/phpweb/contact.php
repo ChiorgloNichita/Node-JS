761849a2b0fdcec74d63360a0dd9050edde3cc73
Chioroglo <niki.kioroglo.2004@gmail.com>
2025-10-12 23:41:11 +0300

    lab2

---
+++ AWS/phpweb/contact.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Контакты</title>
+</head>
+
+<body>
+  <h1>Контакты</h1>
+  <p>Связаться со мной: niki.kioroglo.2004@gmail.com</p>
+  <a href="index.php">Главная</a> |
+  <a href="about.php">О нас</a>
+</body>
+
+</html>
---
+++ AWS/phpweb/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Главная</title>
+</head>
+<body>
+  <h1>Добро пожаловать на мой PHP-сайт!</h1>
+  <p>Это главная страница, обрабатываемая через PHP-FPM.</p>
+  <a href="about.php">О нас</a> |
+  <a href="contact.php">Контакты</a>
+</body>
+</html>
